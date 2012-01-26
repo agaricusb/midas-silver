c86e6e9e82c83d96485a0ba36a2ffb2770d304a3
Laurence Reading <laurence.reading@gmail.com>
2012-01-26 11:37:09 +0000

    making branch

---
+++ branches/mIDas/src/pfaeff/OperatingSystemFamily.java
@@ -0,0 +1,12 @@
+package pfaeff;
+
+//from: http://www.java-tutorial.ch/core-java-tutorial/java-osname-system-property
+//14.07.2011 - 13:47
+
+public enum OperatingSystemFamily {
+    LINUX,
+    WINDOWS,
+    UNIX,
+    DEC_OS,
+    MAC;
+}
