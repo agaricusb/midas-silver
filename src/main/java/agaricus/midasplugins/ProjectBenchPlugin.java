55ce6611415986fd96752026e2f4fba1cf721ee6
Agaricus <agaricusb@yahoo.com>
2013-04-23 19:45:08 -0700

    List tile entities

--- src/main/java/agaricus/midasplugins/ProjectBenchPlugin.java
+++ src/main/java/agaricus/midasplugins/ProjectBenchPlugin.java
@@ -1,11 +1,14 @@
 package agaricus.midasplugins;
 
+import com.mojang.nbt.ListTag;
 import com.mojang.nbt.Tag;
 import havocx42.BlockUID;
 import havocx42.ConverterPlugin;
 import havocx42.PluginType;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ProjectBenchPlugin implements ConverterPlugin {
     @Override
@@ -19,8 +22,24 @@ public class ProjectBenchPlugin implements ConverterPlugin {
     }
 
     @Override
-    public void convert(Tag tag, HashMap<BlockUID, BlockUID> translations) {
-        System.out.println("PB TAG:"+tag);
-        tag.print(System.out);
+    public void convert(Tag root, HashMap<BlockUID, BlockUID> translations) {
+        System.out.println("PB TAG:"+root);
+        root.print(System.out);
+
+        Tag tileEntitiesTag = root.findChildByName("TileEntities", true);
+        if (!(tileEntitiesTag instanceof ListTag)) {
+            System.out.println("TileEntities not list: " + tileEntitiesTag);
+            return;
+        }
+        ListTag tileEntitiesListTag = (ListTag) tileEntitiesTag;
+        System.out.println("PB TE found:"+tileEntitiesListTag.size());
+
+        for (int i = 0; i < tileEntitiesListTag.size(); ++i) {
+            Tag tileEntity = tileEntitiesListTag.get(i);
+
+            Tag idTag = tileEntity.findChildByName("id", false);
+
+            System.out.println(" - idTag="+idTag+" name="+(idTag == null ? "null" : idTag.getName()));
+        }
     }
 }
